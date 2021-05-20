6a0b7ee71c150e439058c5e21bec76a87bfc5374
AhmedMaad <atef.ahmed1341@gmail.com>
2021-05-20 06:20:41 +0200

    login validation is done

--- app/src/main/java/com/example/miniolx/SignInActivity.java
+++ app/src/main/java/com/example/miniolx/SignInActivity.java
@@ -2,13 +2,135 @@ package com.example.miniolx;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private int visibilityCounter = 0;
+    private ProgressBar resetPasswordPB;
+    private Button resetPasswordBtn;
+    private TextInputEditText emailET;
+    private TextInputEditText passwordET;
+    private Button signInBtn;
+    private ProgressBar loginPB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        resetPasswordPB = findViewById(R.id.pb_password_reset);
+        resetPasswordBtn = findViewById(R.id.btn_password_reset_link);
+        emailET = findViewById(R.id.et_email);
+        passwordET = findViewById(R.id.et_password);
+        signInBtn = findViewById(R.id.btn_signin);
+        loginPB = findViewById(R.id.pb_login);
+
+
     }
+
+    public void openSignUp(View view) {
+        Intent i = new Intent(SignInActivity.this, RegisterActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+    public void forgetPassword(View view) {
+        LinearLayout resetContainer = findViewById(R.id.ll_reset_container);
+        if (visibilityCounter % 2 == 0)
+            resetContainer.setVisibility(View.VISIBLE);
+        else
+            resetContainer.setVisibility(View.GONE);
+        ++visibilityCounter;
+    }
+
+    public void sendPasswordResetLink(View view) {
+        TextInputEditText emailET = findViewById(R.id.et_reset_email);
+        String writtenEmail = emailET.getText().toString();
+        Validation validation = new Validation();
+        int validationResult = validation.validateFields(writtenEmail);
+        switch (validationResult) {
+            case 0:
+                Toast.makeText(this, R.string.missing_fileds, Toast.LENGTH_SHORT).show();
+                break;
+            case 1:
+                resetPasswordPB.setVisibility(View.VISIBLE);
+                resetPasswordBtn.setVisibility(View.INVISIBLE);
+                /*UserModel userModel = new UserModel();
+                userModel.resetPassword(writtenEmail, new GeneralHandler() {
+                    @Override
+                    public <T> void onSuccess(Task<T> task) {
+                        loginBinding.pbPasswordReset.setVisibility(View.INVISIBLE);
+                        loginBinding.btnPasswordResetLink.setVisibility(View.VISIBLE);
+                        Toast.makeText(getContext(), R.string.check_email, Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onFailure(Exception e) {
+                        loginBinding.pbPasswordReset.setVisibility(View.INVISIBLE);
+                        loginBinding.btnPasswordResetLink.setVisibility(View.VISIBLE);
+                        Toast.makeText(getContext(), R.string.email_not_recorded, Toast.LENGTH_SHORT).show();
+                        Log.d("json", "Error: " + e.getMessage());
+                    }
+                });*/
+                break;
+        }
+    }
+
+    public void signIn(View view) {
+        String email = emailET.getText().toString();
+        String password = passwordET.getText().toString();
+
+        Validation validation = new Validation();
+        int validationResult = validation.validateFields(email, password);
+        switch (validationResult) {
+            case 0:
+                Toast.makeText(this, R.string.missing_fileds, Toast.LENGTH_SHORT).show();
+                break;
+            case 1:
+                loginPB.setVisibility(View.VISIBLE);
+                signInBtn.setVisibility(View.INVISIBLE);
+                /*UserModel userModel = new UserModel();
+                userModel.loginWithFirebase(email, password, new LoginHandler() {
+                    @Override
+                    public <T> void onSuccess(Task<T> task) {
+                        loginBinding.pbLogin.setVisibility(View.GONE);
+                        loginBinding.btnSignin.setVisibility(View.VISIBLE);
+                        if (task.isSuccessful()) {
+                            Object authResultGeneric = task.getResult();
+                            AuthResult authResult = AuthResult.class.cast(authResultGeneric);
+                            FirebaseUser user = authResult.getUser();
+                            Log.d("json", "Email found: " + user.getEmail());
+                            if (user.isEmailVerified()) {
+                                //Toast.makeText(getContext(), R.string.logged_successfully, Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getContext(), HomeActivity.class);
+                                startActivity(intent);
+                                getActivity().finish();
+                            } else
+                                Toast.makeText(getContext(), R.string.verify_email, Toast.LENGTH_SHORT).show();
+                        } else
+                            Toast.makeText(getContext(), R.string.wrong_email_password, Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onFailure(Exception e) {
+                        loginBinding.pbLogin.setVisibility(View.GONE);
+                        loginBinding.btnSignin.setVisibility(View.VISIBLE);
+                        Log.d("json", "Exception: " + e.getMessage());
+                    }
+                });
+                break;*/
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/miniolx/Validation.java
@@ -0,0 +1,38 @@
+package com.example.miniolx;
+
+public class Validation {
+
+    /**
+     * @return 0: if all the fields are empty or don't meet validity requirements
+     * 1: if all the fields meet validity requirements
+     */
+    public int validateFields(String firstName, String lastName, String username, String email
+            , String password) {
+        if (firstName.equals("") || lastName.equals("") || username.equals("") || email.equals("")
+                || username.length() < 4 || username.length() > 20 || password.equals(""))
+            return 0;
+        else
+            return 1;
+    }
+
+    /**
+     * @return 0: if all the fields are empty or don't meet validity requirements
+     * 1: if all the fields meet validity requirements
+     */
+    public int validateFields(String email, String password) {
+        if (email.equals("") || password.equals(""))
+            return 0;
+        else
+            return 1;
+    }
+
+    /**
+     * @return 0: if all the fields are empty or don't meet validity requirements
+     * 1: if all the fields meet validity requirements
+     */
+    public int validateFields(String text) {
+        return text.equals("") ? 0 : 1;
+    }
+
+
+}
