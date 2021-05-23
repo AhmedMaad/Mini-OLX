9e5c30dc4c51c43ba424526d8553d43dbe57432d
AhmedMaad <atef.ahmed1341@gmail.com>
2021-05-23 18:30:32 +0200

    logging out functionality

--- app/src/main/java/com/example/miniolx/HomeActivity.java
+++ app/src/main/java/com/example/miniolx/HomeActivity.java
@@ -1,8 +1,14 @@
 package com.example.miniolx;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +16,23 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.logout_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.item_logout) {
+            FirebaseAuth.getInstance().signOut();
+            Intent i = new Intent(this, SignInActivity.class);
+            startActivity(i);
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
