a3de9c7192dee2eefa77139cb37892aa01c90f9c
AhmedMaad <atef.ahmed1341@gmail.com>
2021-05-23 18:48:44 +0200

    home design

--- app/src/main/java/com/example/miniolx/HomeActivity.java
+++ app/src/main/java/com/example/miniolx/HomeActivity.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -20,19 +22,29 @@ public class HomeActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.logout_menu, menu);
+        getMenuInflater().inflate(R.menu.my_menu, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.item_logout) {
-            FirebaseAuth.getInstance().signOut();
-            Intent i = new Intent(this, SignInActivity.class);
-            startActivity(i);
-            finish();
-            return true;
+        switch (item.getItemId()) {
+            case R.id.item_logout:
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(this, R.string.logged_out, Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(this, SignInActivity.class);
+                startActivity(i);
+                finish();
+                return true;
+            case R.id.item_my_apartments:
+                startActivity(new Intent(this, MyApartments.class));
+                return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void openAddProductActivity(View view) {
+        Intent i = new Intent(this, AddProductActivity.class);
+        startActivity(i);
     }
 }
