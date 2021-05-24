addc6602094cc2eb2aa0e450f2330dbf533ae310
AhmedMaad <atef.ahmed1341@gmail.com>
2021-05-24 15:05:08 +0200

    home design

--- app/src/main/java/com/example/miniolx/HomeActivity.java
+++ app/src/main/java/com/example/miniolx/HomeActivity.java
@@ -2,23 +2,69 @@ package com.example.miniolx;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private ProgressBar progressBar;
+    private EditText searchET;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         setTitle("Available Apartments");
+
+        recyclerView = findViewById(R.id.rv);
+        progressBar = findViewById(R.id.pb);
+        searchET = findViewById(R.id.et_search_place);
+        RadioGroup rg = findViewById(R.id.rg);
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.rb_low_to_high:
+                        //call filter from low to high method
+                        break;
+                    case R.id.rb_high_to_low:
+                        //call filter from high to low method
+                        break;
+                }
+            }
+        });
+        searchET.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                //call filter by name
+            }
+        });
+
     }
 
     @Override
