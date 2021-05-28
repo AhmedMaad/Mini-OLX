a1c4badea64075232dedd9fc566b26edef14c708
AhmedMaad <atef.ahmed1341@gmail.com>
2021-05-28 04:29:34 +0200

    splash screen is added

--- app/src/main/java/com/example/miniolx/activities/SplashActivity.java
+++ app/src/main/java/com/example/miniolx/activities/SplashActivity.java
@@ -2,7 +2,10 @@ package com.example.miniolx.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 import com.example.miniolx.R;
 
@@ -12,5 +15,22 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+        View decorView = getWindow().getDecorView();
+// Hide both the navigation bar and the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        int delayTime = 3500;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, SignInActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, delayTime);
+
     }
 }
